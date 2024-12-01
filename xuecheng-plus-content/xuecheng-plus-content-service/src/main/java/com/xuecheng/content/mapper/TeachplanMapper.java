8682caf9f76de6b296b1f806923436e4c4bd97b9
YRMLunar <198003617@qq.com>
2024-12-01 20:36:54 +0800

    提交课程查询接口定义代码

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.TeachPlanDto;
+import com.xuecheng.content.model.po.Teachplan;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 课程计划 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface TeachplanMapper extends BaseMapper<Teachplan> {
+
+    List<TeachPlanDto> selectTreeNodes(long courseId);
+
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,41 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.*;
+import com.xuecheng.content.model.po.CourseBase;
+
+import java.util.List;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 课程信息管理接口
+ * @date 2023/2/12 10:14
+ */
+public interface CourseBaseInfoService {
+
+    /**
+     * 课程分页查询
+     * @param pageParams 分页查询参数
+     * @param courseParamsDto 查询条件
+     * @return 查询结果
+     */
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto courseParamsDto);
+
+    /**
+     * 新增课程
+     * @param companyId 机构id
+     * @param addCourseDto 课程信息
+     * @return 课程详细信息
+     */
+    public CourseBaseInfoDto createCourseBase(Long companyId,AddCourseDto addCourseDto);
+
+    CourseBaseInfoDto getCourseBaseInfo(long id);
+
+    CourseBaseInfoDto updateCouseBaseInfo(Long companyId, EditCourseDto editCourseDto);
+
+
+
+    void deleteCourse(Long courseId, Long compneyId);
+}
